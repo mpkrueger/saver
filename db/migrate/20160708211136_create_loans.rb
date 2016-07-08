3e930d9b6a8d2324ae5f03ec923bb83a2a9f66db
Matt Krueger <mpkrueger@gmail.com>
2016-07-08 14:20:32 -0700

    created model for loan data

---
+++ db/migrate/20160708211136_create_loans.rb
@@ -0,0 +1,11 @@
+class CreateLoans < ActiveRecord::Migration[5.0]
+  def change
+    create_table :loans do |t|
+      t.decimal :balance
+      t.decimal :rate
+      t.decimal :months_remaining
+
+      t.timestamps
+    end
+  end
+end
