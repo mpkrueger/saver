3cc42d2245133ac8201089a32c69dfba0ac53913
Matt Krueger <mpkrueger@gmail.com>
2016-07-08 15:46:14 -0700

    loan info is saved and displayed on new page

---
+++ app/controllers/loans_controller.rb
@@ -0,0 +1,29 @@
+class LoansController < ApplicationController
+  def index
+  	@loans = Loan.all
+  end
+
+  def show
+  	@loan = Loan.find(params[:id])
+  end
+
+  def create
+  	@loan = Loan.new(params.require(:loan).permit(:balance, :rate, :months_remaining))
+  	# TO DO create user account to tie loan to
+  	# @loan.user = current_user
+
+  	if @loan.save
+  		flash[:notice] = "Loan info saved"
+  		redirect_to @loan
+  	else
+  		flash[:error] = "uh oh"
+  		render :new
+  	end
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
