a47bf9976be49464dc4d81e4ab958f2f951951f7
Matt Krueger <mpkrueger@gmail.com>
2016-07-08 10:59:24 -0700

    Updated home page copy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "<h1>Hello</h1><p>Welcome</p>"
+  	render text: 
+  		"<h1>Saver: Helping you reach (and savor) your financial goals</h1>
+  			<p>Which of these is a goal you'd like to achieve?</p>
+  			<li>Paying off my student loan</li>
+  			<li>Buying a house</li>
+  			<li>Saving for retirement</li>"
   end
 end
