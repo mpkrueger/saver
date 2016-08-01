1921756c87b9fae4d580261fbe5c2f8b703555f2
Sushmita <ssubrama@gmail.com>
2016-08-01 12:02:49 -0700

    rubified

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,31 @@
 
 ActiveRecord::Schema.define(version: 20160730204219) do
 
+  create_table "houses", force: :cascade do |t|
+    t.decimal  "budget"
+    t.decimal  "down_payment"
+    t.decimal  "years_owning"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.decimal  "balance"
+    t.decimal  "rate"
+    t.decimal  "months_remaining"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "retirements", force: :cascade do |t|
+    t.decimal  "savings"
+    t.decimal  "age"
+    t.decimal  "years_remaining"
+    t.decimal  "annual_spend"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "why_here"
     t.string   "money_feels"
