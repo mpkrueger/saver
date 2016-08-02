638c48e879b29c02c56a4914135a712154ed6a15
Sushmita <ssubrama@gmail.com>
2016-08-02 16:51:46 -0700

    added logic for savings message on investments page

--- app/helpers/intro_helper.rb
+++ app/helpers/intro_helper.rb
@@ -12,4 +12,26 @@ module IntroHelper
 			"That’s rough, you must be eager to get those off your back."
 		end
 	end
+
+	def savings_feedback
+		@user = @current_user
+		case @user.savings_amount
+		when 0
+			"Cool, we’ll work on that. 46 of Americans wouldn’t be able to cover a sudden $400 expense so you’re not alone."
+		when 1
+			"Cool, we’ll work on increasing that. It’s generally good to have about 3-6 months worth of expenses saved up."
+		when 2, 3
+			"That’s great, glad to hear you’re on the right track."
+		end
+	end
+
+	def investment_question
+		@user = @current_user
+		case @user.savings_amount
+		when 0, 1
+			"Do you have any investments?"
+		when 2, 3
+			"Are you putting any of your money to work?"
+		end
+	end
 end
