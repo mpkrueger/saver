bd1913e1014adc86a153fcdf0caa6a7ad4a250fe
Matt Krueger <mpkrueger@gmail.com>
2016-08-10 14:30:40 -0700

    creating feedback table, model and controller

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,18 @@
+class FeedbacksController < ApplicationController
+	def new
+		@user = current_user
+		@feedback = Feedback.new
+	end
+
+	def create
+		@feedback = Feedback.new(params.require(:feedback).permit(:nps, :trust, :comment))
+		@feedback.user = current_user
+
+		if @feedback.save
+			redirect_to root_path
+		else
+			flash[:error] = "uh oh, there was an error - please try again or let us know!"
+			redirect_to dashboard_thanks_path
+		end
+	end
+end
