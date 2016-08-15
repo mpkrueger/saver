ef9206a61d07a057439bf81c672504477cb581c2
Matt Krueger <mpkrueger@gmail.com>
2016-08-15 13:06:51 -0700

    fixing dashboard start button so that it is based on current_focus

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,7 @@
 module DashboardHelper
 
 	def homepage_challenge_button
-		case @user.fin_profile.area_to_work_on
+		case @user.fin_profile.current_focus
 		when "SAVINGS HABITS", "SAVINGS"
 			challenges_saving_info_path
 		when "STUDENTS LOANS"
