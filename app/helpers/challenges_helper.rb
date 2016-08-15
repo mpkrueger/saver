e4e7bb9df8869c2f6bf6fc1a40096d152bd338cc
Sushmita <ssubrama@gmail.com>
2016-08-14 22:22:04 -0700

    some calculations for the cc model page

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,2 +1,17 @@
 module ChallengesHelper
+
+	def daily_interest_rate
+		@user = current_user
+		@daily_interest_calc = (@user.fin_profile.cc_rate / 365).round(2)
+	end
+
+	def daily_interest_amount
+		@user = current_user
+		@daily_amount_calc = (@user.fin_profile.cc_amount * daily_interest_rate).round(2)	
+	end
+
+	def new_daily_balance
+		@user = current_user
+		@new_example_balance = (@user.fin_profile.cc_amount + daily_interest_amount).round(2)
+	end
 end
