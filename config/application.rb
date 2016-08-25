1f1bea5490ff4cce9fea7e1bdacc16c2f63365d1
Matt Krueger <mpkrueger@gmail.com>
2016-08-25 09:52:42 -0700

    setting time zone for the app to pacific time zone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module FinGoals
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Setting time zone for app to West Coast US
+	config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
