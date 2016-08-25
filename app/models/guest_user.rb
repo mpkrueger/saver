955c83757368bb28574ad0d7d64abc37f33ad1df
Matt Krueger <mpkrueger@gmail.com>
2016-08-24 17:18:51 -0700

    fixing models for scoring savings habits

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -105,13 +105,11 @@ class GuestUser < ApplicationRecord
 		savings_habits = 0
 
 		case self.savings_from_income
-		when "very little"
+		when "a lot"
 			savings_habits += 15
-		when "some"
-			savings_habits += 10
-		when "most"
+		when "a little"
 			savings_habits += 5
-		when "all"
+		when "none"
 			savings_habits += 0
 		end
 
