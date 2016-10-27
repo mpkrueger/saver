cbc2d44aaa3d69d3836fb93823ec2ccd4bcb384d
Matt Krueger <mpkrueger@gmail.com>
2016-10-27 12:14:15 -0700

    stopping the old intro email from sending on user account creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -38,7 +38,6 @@ class RegistrationsController < Devise::RegistrationsController
       	if resource.active_for_authentication?
 	        set_flash_message! :notice, :signed_up
 	        sign_up(resource_name, resource)
-	        BetaMailer.beta_intro(resource).deliver
 	        respond_with resource, location: after_sign_up_path_for(resource)
 	    else
 	        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
