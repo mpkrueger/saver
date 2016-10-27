8d5a2b8f85207932f2cafb6a54075d30d45eb0a5
Matt Krueger <mpkrueger@gmail.com>
2016-10-27 09:31:04 -0700

    generating new investing mailer for investing emails

---
+++ test/mailers/investing_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InvestingMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/investing_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/investing_mailer
+class InvestingMailerPreview < ActionMailer::Preview
+
+end
