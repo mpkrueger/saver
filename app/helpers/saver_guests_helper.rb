f5c332bedd78feb9d73c746ab8c7c7aa6b67ed30
Sushmita <ssubrama@gmail.com>
2016-12-08 21:06:47 -0800

    added fixed header and updated copy and styling

--- app/helpers/saver_guests_helper.rb
+++ app/helpers/saver_guests_helper.rb
@@ -2,9 +2,9 @@ module SaverGuestsHelper
 	def projected_savings
 		if(@saver_guest.last_moved == 'more than a year ago' && @saver_guest.negotiated == "no")
 			if(@saver_guest.services['phone']=='1' && @saver_guest.services['internet']=='1' && @saver_guest.services['cable']=='1')
-				"$500"
+				"$500/year"
 			else
-				"$300"
+				"$300/year"
 			end
 		else
 			"–"
