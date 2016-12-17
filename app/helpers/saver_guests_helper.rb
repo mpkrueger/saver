a9eb3212b363fde8319b39ec3cc98a4fc238ecf5
Sushmita <ssubrama@gmail.com>
2016-12-16 16:25:23 -0800

    removed negotiated reference

--- app/helpers/saver_guests_helper.rb
+++ app/helpers/saver_guests_helper.rb
@@ -1,6 +1,6 @@
 module SaverGuestsHelper
 	def projected_savings
-		if(@saver_guest.last_moved == 'More than a year ago' && @saver_guest.negotiated == "No")
+		if(@saver_guest.last_moved == 'More than a year ago')
 			if(@saver_guest.services['phone']=='1' && @saver_guest.services['internet']=='1' && @saver_guest.services['cable']=='1')
 				"$500/year"
 			else
