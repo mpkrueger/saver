90b65ca797d04bf9673300cea83aec1b933480bc
Matt Krueger <mpkrueger@gmail.com>
2016-12-20 13:44:12 -0800

    removing logic in email based on services

--- app/mailers/saver_guest_mailer.rb
+++ app/mailers/saver_guest_mailer.rb
@@ -10,15 +10,7 @@ class SaverGuestMailer < ApplicationMailer
     
     @subject = "Send us your bill so we can save you money"
 
-    if(@user.last_moved == 'More than a year ago')
-      if(@user.services['phone']=='1' && @user.services['internet']=='1' && @user.services['cable']=='1')
-        @custom_message = "You mentioned that you have all 3 services - cable tv, internet, and phone - which means we're confident we can save you more than $500 per year."
-      else
-        
-      end
-    elsif @user.last_moved == 'Less than a year ago'
-      @custom_message = "Because you’ve moved in the last year, there may be less of an opportunity to negotiate a lower price - your latest bill will tell us a lot."
-    end
+    
 
 
     mail(to: @user.email, subject: @subject)
