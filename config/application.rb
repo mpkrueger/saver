7b9074edba2594b47784b9e0c56975beaefcdf45
Sushmita <ssubrama@gmail.com>
2016-12-25 22:42:38 -0800

    fixing active admin style overriding

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# Bundler.require(:default, :assets, Rails.env)
 Bundler.require(*Rails.groups)
 
 module FinGoals
@@ -14,5 +15,12 @@ module FinGoals
 
     # Setting time zone for app to West Coast US
 	config.time_zone = 'Pacific Time (US & Canada)'
+
+	# assets pipeline config to work on Heroku
+    # config.assets.initialize_on_precompile = false
+
+    # add active admin assets to precompile list, loaded from vendor/assets
+    #config.assets.precompile += %w( active_admin.js active_admin.css.scss )
+
   end
 end
