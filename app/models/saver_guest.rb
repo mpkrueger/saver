77413fa78b30fe1f86350efafcce0c7b9bc470bb
mpkrueger <mpkrueger@gmail.com>
2017-01-03 11:48:06 -0800

    adding method to create tickets for each of our existing saver guests

--- app/models/saver_guest.rb
+++ app/models/saver_guest.rb
@@ -1,4 +1,10 @@
 class SaverGuest < ApplicationRecord
 	has_many :tickets
 	has_many :bills, through: :tickets
+
+	def create_ticket
+		@ticket = Ticket.new
+		@ticket.saver_guest = self
+		@ticket.save
+	end
 end
