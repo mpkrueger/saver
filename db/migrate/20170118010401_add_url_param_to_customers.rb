7ebdce2db37b3b34552b403e5ce26bf41ceba434
mpkrueger <mpkrueger@gmail.com>
2017-01-17 17:32:46 -0800

    changing the last migration to make sure url param is unique

--- db/migrate/20170118010401_add_url_param_to_customers.rb
+++ db/migrate/20170118010401_add_url_param_to_customers.rb
@@ -1,5 +1,5 @@
 class AddUrlParamToCustomers < ActiveRecord::Migration[5.0]
   def change
-    add_column :customers, :invite_url_param, :string
+    add_column :customers, :invite_url_param, :string, unique: true
   end
 end
