f69be9fae0678ede23e2b2424d2027ec41b0b0a0
Sushmita <ssubrama@gmail.com>
2017-01-19 17:09:46 -0800

    updates to referral page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -138,7 +138,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    devise_parameter_sanitizer.sanitize(:sign_up)
+    params.require(:customer).permit(:first_name, :email, :password)
   end
 
   def account_update_params
