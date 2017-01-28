b6d2a8228cb1794e345ca2210e18c0d37cfc3ba3
mpkrueger <mpkrueger@gmail.com>
2017-01-27 17:25:19 -0800

    creating method to create customers for tickets without

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,18 @@ class Ticket < ApplicationRecord
   has_many :bills
   has_many :calls
   strip_attributes
+
+  def create_customer
+  	if self.customer.nil?
+  		@customer = Customer.new(
+  			email: "sg+#{self.saver_guest.email}",
+  			invite_url_param: "sg+#{self.saver_guest.email.split("@").first}",
+  			first_name: self.saver_guest.name,
+  			password: "password+#{rand(1000)}"
+  			)
+  		self.customer = @customer
+  		@customer.save
+  	end
+  end
+
 end
