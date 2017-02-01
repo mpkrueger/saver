abdda8d5e8dfae09657632e75d1aab384671cacd
Sushmita <ssubrama@gmail.com>
2017-02-01 14:27:20 -0800

    adding FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
 
+  get 'about_us', to: 'info#about_us', as: 'about_us'
+  get 'faq', to: 'info#faq', as:'faq'
+  get 'privacy_policy', to: 'info#privacy_policy', as: 'privacy_policy'
+  get 'terms_of_service', to: 'info#terms_of_service', as: 'terms_of_service'
+
   authenticated :customer do
     root to: 'dashboards#dashboard'
   end
