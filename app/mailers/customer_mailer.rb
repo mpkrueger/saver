0b48b449f2569189f7ea0d4f4b2608b248e424d0
mpkrueger <mpkrueger@gmail.com>
2017-02-01 12:52:32 -0800

    switching from inline attachment images to straight linked images

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -8,9 +8,6 @@ class CustomerMailer < ApplicationMailer
   def signup_bill(customer)
     @customer = customer
 
-    attachments.inline['lp_hero_image.png'] = File.read("#{Rails.root}/app/assets/images/lp_hero_image.png")
-    attachments.inline['fb_circle_icon.png'] = File.read("#{Rails.root}/app/assets/images/fb_circle_icon.png")
-    attachments.inline['twitter_circle_icon.png'] = File.read("#{Rails.root}/app/assets/images/twitter_circle_icon.png")
     
     @subject = "Send us your bill so we can save you money"
 
