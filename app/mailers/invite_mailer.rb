b4ae3099e6dc13740ff57f222dae06fef108209b
mpkrueger <mpkrueger@gmail.com>
2017-02-01 11:22:14 -0800

    adding images as inline attachments on emails

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -11,6 +11,10 @@ class InviteMailer < ApplicationMailer
 
     @subject = "#{@invite.customer.first_name} wants to help you save money with no effort"
 
+    attachments.inline['lp_hero_image.png'] = File.read("#{Rails.root}/app/assets/images/lp_hero_image.png")
+    attachments.inline['fb_circle_icon.png'] = File.read("#{Rails.root}/app/assets/images/fb_circle_icon.png")
+    attachments.inline['twitter_circle_icon.png'] = File.read("#{Rails.root}/app/assets/images/twitter_circle_icon.png")
+
     mail(to: @invite.receiver_email, subject: @subject)
   end
 
