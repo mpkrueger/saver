e1cf0d75511152cf41511eac86e36854131a68b3
mpkrueger <mpkrueger@gmail.com>
2017-02-03 12:54:01 -0800

    updating the invites admin page to show relevant info

--- app/admin/invite.rb
+++ app/admin/invite.rb
@@ -17,4 +17,21 @@ ActiveAdmin.register Invite do
 		belongs_to :customer, optional: true
 	end
 
+	index do
+		selectable_column
+		column("Invite ID", :sortable => :id) {|invite| link_to "##{invite.id} ", admin_invite_path(invite) }
+	    # column("State")                   {|ticket| status_tag(ticket.state) }
+	  	column("Sender Name") {|invite| invite.customer.first_name}
+	  	column("Sender Email") {|invite| invite.customer.email }
+	  	column("Receiver Email") {|invite| invite.receiver_email }
+	    column "Receiver Customer" do |invite|
+	    	if Customer.find_by_email(invite.receiver_email)
+	      		link_to "#{Customer.find_by_email(invite.receiver_email).first_name} ", admin_customer_path(Customer.find_by_email(invite.receiver_email))
+	      	else
+	      		"No customer yet"
+	      	end
+	    end
+	    
+	end
+
 end
