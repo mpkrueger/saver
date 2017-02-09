b868bf7abdb6e78eaa7f846562c28c186b096dfa
Matt Krueger <mpkrueger@gmail.com>
2017-02-09 14:35:55 -0800

    moving to 5 weeks of history on admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,19 +5,19 @@ ActiveAdmin.register_page "Dashboard" do
   content do
     panel "New tickets by week" do
       #  line_chart SaverGuest.group_by_day(:created_at, last: 30).count, xtitle: "Date", ytitle: "Saver Guests"
-        bar_chart Ticket.group_by_week(:created_at, last:10).count, xtitle:"New Tickets", ytitle: "By Week"
+        bar_chart Ticket.group_by_week(:created_at, last:5).count, xtitle:"New Tickets", ytitle: "By Week"
     end
 
     panel "Percent of new tickets w/ bills collected by week" do
-        bar_chart Ticket.group_by_week(:created_at, last:10).where("has_bill" == true).count, xtitle:"Bills Collected", ytitle:"By Week"
+        bar_chart Ticket.group_by_week(:created_at, last:5).where("has_bill" == true).count, xtitle:"Bills Collected", ytitle:"By Week"
     end
 
     panel "Amount earned by week" do
-        bar_chart Ticket.group_by_week(:created_at, last:10).where("successfully_saved_money" == true).sum(:amount_earned), xtitle:"Bills Collected", ytitle:"By Week"
+        bar_chart Ticket.group_by_week(:created_at, last:5).where("successfully_saved_money" == true).sum(:amount_earned), xtitle:"Amount earned", ytitle:"By Week"
     end
 
     panel "Overall Stats" do
-        para "Average saved: #{ monetize( Ticket.where(has_bill: true).average(:amount_saved).round(2) ) }"
+        para "Average saved: #{ monetize( Ticket.where(has_bill: true).average(:amount_saved) ) }"
         para "Total saved: #{ monetize(Ticket.sum(:amount_saved)) }"
         para "Total earned: #{ monetize(Ticket.sum(:amount_earned))}"
     end
