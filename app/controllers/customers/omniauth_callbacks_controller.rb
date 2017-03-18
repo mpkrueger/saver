1efa89b44d0293537b69cfe29e6a097b3938c5da
mpkrueger <mpkrueger@gmail.com>
2017-03-18 15:39:52 -0700

    getting rid of flash message on successful facebook signup

--- app/controllers/customers/omniauth_callbacks_controller.rb
+++ app/controllers/customers/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class Customers::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
 
     	if @customer.persisted?
       		sign_in_and_redirect @customer #this will throw if @customer is not activated
-      		set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      		
     	else
       		session["devise.facebook_data"] = request.env["omniauth.auth"]
       		redirect_to new_customer_registration_url
