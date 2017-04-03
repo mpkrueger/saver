f09448b123542df53f98eea3f3b176dade6a2ab9
Sushmita <ssubrama@gmail.com>
2017-04-03 16:59:59 -0700

    landing page for guys

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   get '3', to: 'onboarding#landing-page-vector'
   get '4', to: 'onboarding#landing-page-painpoints'
   get '5', to: 'onboarding#landing-page-busymom'
-  get '6', to: 'onboarding#landing-page-education'
+  get '6', to: 'onboarding#landing-page-busyguy'
 
   devise_for :customers, controllers: { registrations: "registrations", omniauth_callbacks: "customers/omniauth_callbacks" }, path: '', path_names: { sign_up: 'sign-up', sign_in: 'sign-in' }
   devise_for :admin_users, ActiveAdmin::Devise.config 
